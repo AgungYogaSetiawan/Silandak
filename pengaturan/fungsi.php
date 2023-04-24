7cde7099166d99eb137a5e0a534ad7352a2e6dcd
Agung Yoga Setiawan <agungyoga507@gmail.com>
2023-04-24 09:21:43 +0800

    membuat fungsi crud

--- pengaturan/fungsi.php
+++ pengaturan/fungsi.php
@@ -26,3 +26,52 @@ function registrasi($data){
 
   return mysqli_affected_rows($conn);
 }
+
+// fungsi tambah data
+function create($table, $data) {
+  global $conn;
+
+  $fields = implode(',', array_keys($data));
+  $values = implode(',', array_map(function ($value) {
+      return "'" . $value . "'";
+  }, array_values($data)));
+  $query = "INSERT INTO $table ($fields) VALUES ($values)";
+  $result = mysqli_query($conn,$query);
+  return $result;
+}
+
+
+// fungsi tampil data
+function read($table, $id = null) {
+  global $conn;
+
+  $query = "SELECT * FROM $table";
+  if ($id) {
+      $query .= " WHERE id = $id";
+  }
+  $result = mysqli_query($conn,$query);
+  return $result->fetch_all(MYSQLI_ASSOC);
+}
+
+
+// fungsi edit data
+function update($table, $id, $data) {
+  global $conn;
+
+  $set = implode(',', array_map(function ($key, $value) {
+      return $key . "='" . $value . "'";
+  }, array_keys($data), array_values($data)));
+  $query = "UPDATE $table SET $set WHERE id = $id";
+  $result = mysqli_query($conn,$query);
+  return $result;
+}
+
+
+// fungsi hapus data
+function delete($table, $id) {
+  global $conn;
+
+  $query = "DELETE FROM $table WHERE id = $id";
+  $result = mysqli_query($conn,$query);
+  return $result;
+}
