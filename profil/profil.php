ac5a30e4332c7ca1912b297cf45f23043dc0d896
Agung Yoga Setiawan <agungyoga507@gmail.com>
2023-04-27 12:35:37 +0800

    menambahkan kode update di profil.php

--- profil/profil.php
+++ profil/profil.php
@@ -1,9 +1,44 @@
+<!-- kode php -->
+<?php
+$id = $_SESSION['id'];
+$sql = "SELECT * FROM tb_pemohon a INNER JOIN tb_user b ON a.user_id = b.id_user WHERE b.id_user='$id'";
+$result = mysqli_query($conn,$sql);
+$data = mysqli_fetch_array($result);
+
+if(isset($_POST['simpan'])){
+  $id_pemohon = htmlspecialchars($_POST['id_pemohon']);
+  $kewarganegaraan = htmlspecialchars($_POST['kewarganegaraan']);
+  $nama = htmlspecialchars($_POST['nama']);
+  $nik = htmlspecialchars($_POST['nik']);
+  $no_hp = htmlspecialchars($_POST['no_hp']);
+  $pekerjaan = htmlspecialchars($_POST['pekerjaan']);
+  $tmpt_lahir = htmlspecialchars($_POST['tmpt_lahir']);
+  $tgl_lahir = $_POST['tgl_lahir'];
+  $jk = htmlspecialchars($_POST['jk']);
+  $status = htmlspecialchars($_POST['status']);
+  $agama = htmlspecialchars($_POST['agama']);
+  $kelurahan = htmlspecialchars($_POST['kelurahan']);
+  $kecamatan = htmlspecialchars($_POST['kecamatan']);
+  $rt = htmlspecialchars($_POST['rt']);
+  $rw = htmlspecialchars($_POST['rw']);
+  $alamat = htmlspecialchars($_POST['alamat']);
+  $kode_pos = htmlspecialchars($_POST['kode_pos']);
+  $foto = htmlspecialchars($_POST['foto']);
+
+  $sql = "UPDATE tb_pemohon SET kewarganegaraan = '$kewarganegaraan', nama = '$nama', nik = '$nik', no_hp = '$no_hp', pekerjaan = '$pekerjaan', tmpt_lahir = '$tmpt_lahir', tgl_lahir = '$tmgl_lahir', jk = '$jk', status = '$status', agama = '$agama', kelurahan = '$kelurahan', kecamatan = '$kecamatan', rt = '$rt', rw = '$rw', alamat = '$alamat', kode_pos = '$kode_pos', foto = '$foto' WHERE id_pemohon = '$id_pemohon'";
+  mysqli_query($conn, $sql);
+
+  echo "<script>alert('Data Berhasil Disimpan');</script>";
+}
+?>
+<!-- end kode php -->
+
 <!-- Main Content -->
 <div class="main-content">
   <section class="section">
     <div class="section-header justify-content-between bg-danger rounded-lg">
       <h1 class="text-white">FORM DATA PEMOHON</h1>
-      <a href="#"><h1 class="text-white"><i class="fas fa-arrow-left" style="font-size:20px;"></i> Kembali</h1></a>
+      <a href="beranda"><h1 class="text-white"><i class="fas fa-arrow-left" style="font-size:20px;"></i> Kembali</h1></a>
     </div>
 
     <div class="section-body">
@@ -13,11 +48,11 @@
             <div class="card card-danger">
               <div class="card-header text-danger"><h6>DATA PEMOHON</h6></div>
               <div class="card-body">
-                <form method="POST">
+                <form method="POST" enctype="multipart/form-data">
                   <div class="row">
                     <div class="form-group col-6">
                       <label for="kewarganegaraan">Kewarganegaraan</label>
-                      <input id="kewarganegaraan" type="text" class="form-control" name="kewarganegaraan" autofocus>
+                      <input id="kewarganegaraan" type="text" class="form-control" name="kewarganegaraan">
                     </div>
                     <div class="form-group col-6">
                       <label for="nama">Nama Lengkap</label>
@@ -28,7 +63,7 @@
                   <div class="row">
                     <div class="form-group col-6">
                       <label for="nik">NIK</label>
-                      <input id="nik" type="text" class="form-control" name="nik">
+                      <input id="nik" type="number" class="form-control" name="nik">
                       <div class="invalid-feedback">
                     </div>
                   </div>
@@ -48,8 +83,8 @@
                     </div>
                   </div>
                     <div class="form-group col-6">
-                      <label for="tempat_lahir">Tempat Lahir</label>
-                      <input id="tempat_lahir" type="text" class="form-control" name="tempat_lahir">
+                      <label for="tmpt_lahir">Tempat Lahir</label>
+                      <input id="tmpt_lahir" type="text" class="form-control" name="tmpt_lahir">
                       <div class="invalid-feedback">
                       </div>
                     </div>
@@ -65,11 +100,11 @@
                     <div class="form-group col-6">
                       <label class="d-block" for="jk">Jenis Kelamin</label>
                       <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" id="jk" value="">
+                        <input class="form-check-input" type="radio" id="jk" value="Laki-laki">
                         <label class="form-check-label" for="jk">Laki-laki</label>
                       </div>
                       <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" id="jk" value="">
+                        <input class="form-check-input" type="radio" id="jk" value="Perempuan">
                         <label class="form-check-label" for="jk">Perempuan</label>
                       </div>
                     </div>
@@ -80,8 +115,8 @@
                       <label>Status</label>
                       <select class="form-control selectric">
                         <option selected>--Pilih Status--</option>
-                        <option>West Java</option>
-                        <option>East Java</option>
+                        <option>Belum Kawin</option>
+                        <option>Sudah Kawin</option>
                       </select>
                     </div>
                     <div class="form-group col-6">
@@ -103,9 +138,7 @@
                     <div class="form-group col-6">
                       <label>Kecamatan</label>
                       <select class="form-control selectric">
-                        <option selected>--Pilih Kecamatan--</option>
-                        <option>West Java</option>
-                        <option>East Java</option>
+                        <option selected>Banua Lawas</option>
                       </select>
                     </div>
                   </div>
@@ -113,13 +146,13 @@
                   <div class="row">
                     <div class="form-group col-6">
                       <label for="rt">RT</label>
-                      <input id="rt" type="number" class="form-control" name="rt">
+                      <input id="rt" type="text" class="form-control" name="rt">
                       <div class="invalid-feedback">
                       </div>
                     </div>
                     <div class="form-group col-6">
                       <label for="rw">RW</label>
-                      <input id="rw" type="number" class="form-control" name="rw">
+                      <input id="rw" type="text" class="form-control" name="rw">
                       <div class="invalid-feedback">
                       </div>
                     </div>
@@ -128,30 +161,30 @@
                   <div class="row">
                     <div class="form-group col-6">
                       <label>Alamat Domisili</label>
-                      <textarea class="form-control"></textarea>
+                      <textarea class="form-control" name="alamat" id="alamat"></textarea>
                     </div>
                     <div class="form-group col-6">
-                      <label for="kodepos">Kode Pos</label>
-                      <input id="kodepos" type="number" class="form-control" name="kodepos">
+                      <label for="kode_pos">Kode Pos</label>
+                      <input id="kode_pos" type="number" class="form-control" name="kode_pos">
                       <div class="invalid-feedback">
                       </div>
                     </div>
                   </div>
                   <div class="form-group">
-                    <label>Upload Foto Profil</label>
+                    <label>Unggah Foto Profil</label>
                     <div class="custom-file">
                       <input type="file" class="custom-file-input" id="customFile">
-                      <label class="custom-file-label" for="customFile">Choose file</label>
+                      <label class="custom-file-label" for="customFile">Unggah Foto Profil</label>
                     </div>
                   </div>
 
                   <div class="form-group">
-                    <button type="submit" class="btn btn-success btn-md">
+                    <button type="submit" class="btn btn-success btn-md" name="simpan">
                       <i class="fas fa-save"></i> Simpan
                     </button>
-                    <button type="submit" class="btn btn-danger btn-md">
+                    <a href="beranda" class="btn btn-danger btn-md">
                       <i class="fas fa-window-close"></i> Batal
-                    </button>
+                    </a>
                   </div>
                 </form>
               </div>
