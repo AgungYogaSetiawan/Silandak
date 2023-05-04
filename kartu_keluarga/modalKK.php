42f17789b82495c31192bcad9af55ce133eb98cf
Agung Yoga Setiawan <agungyoga507@gmail.com>
2023-05-04 11:33:07 +0800

    membuat view data pemohon kk

--- kartu_keluarga/modalKK.php
+++ kartu_keluarga/modalKK.php
@@ -247,7 +247,7 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="modalLihatDataKKLabel">Form Pendaftaran Kartu Keluarga</h5>
+        <h5 class="modal-title" id="modalLihatDataKKLabel">Permohonan Kartu Keluarga</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -256,27 +256,34 @@
       <div class="modal-body" id="bodydetail">
         <div class="text-danger mb-5"><h6><i class="fas fa-user"></i> DATA PEMOHON</h6></div>
         <form method="POST">
+          <?php
+          $id = $_SESSION['id'];
+          $sql = "SELECT * FROM tb_kk a INNER JOIN tb_user b ON a.user_id = b.id_user WHERE b.id_user='$id'";
+          $result = mysqli_query($conn,$sql);
+          $data = mysqli_fetch_array($result);
+          ?>
+          <input type="hidden" id="user_id" name="user_id" value="<?php echo $data['id_user']; ?>" readonly>
           <div class="row">
             <div class="form-group col-6">
               <label for="kewarganegaraan">Kewarganegaraan</label>
-              <input id="kewarganegaraan" type="text" class="form-control" name="kewarganegaraan" autofocus>
+              <input id="kewarganegaraan" type="text" class="form-control" name="kewarganegaraan" value="<?php echo $data['kewarganegaraan']; ?>">
             </div>
             <div class="form-group col-6">
               <label for="nama">Nama Lengkap</label>
-              <input id="nama" type="text" class="form-control" name="nama">
+              <input id="nama" type="text" class="form-control" name="nama" value="<?php echo $data['nama']; ?>">
             </div>
           </div>
           
           <div class="row">
             <div class="form-group col-6">
               <label for="nik">NIK</label>
-              <input id="nik" type="text" class="form-control" name="nik">
+              <input id="nik" type="text" class="form-control" name="nik" value="<?php echo $data['nik']; ?>">
               <div class="invalid-feedback">
             </div>
           </div>
             <div class="form-group col-6">
               <label for="no_hp">No.Telepon</label>
-              <input id="no_hp" type="number" class="form-control" name="no_hp">
+              <input id="no_hp" type="text" class="form-control" name="no_hp" value="<?php echo $data['no_hp']; ?>">
               <div class="invalid-feedback">
               </div>
             </div>
@@ -285,13 +292,13 @@
           <div class="row">
             <div class="form-group col-6">
               <label for="pekerjaan">Pekerjaan</label>
-              <input id="pekerjaan" type="text" class="form-control" name="pekerjaan">
+              <input id="pekerjaan" type="text" class="form-control" name="pekerjaan" value="<?php echo $data['pekerjaan']; ?>">
               <div class="invalid-feedback">
             </div>
           </div>
             <div class="form-group col-6">
-              <label for="tempat_lahir">Tempat Lahir</label>
-              <input id="tempat_lahir" type="text" class="form-control" name="tempat_lahir">
+              <label for="tmpt_lahir">Tempat Lahir</label>
+              <input id="tmpt_lahir" type="text" class="form-control" name="tmpt_lahir" value="<?php echo $data['tmpt_lahir']; ?>">
               <div class="invalid-feedback">
               </div>
             </div>
@@ -300,18 +307,18 @@
           <div class="row">
             <div class="form-group col-6">
               <label for="tgl_lahir">Tanggal Lahir</label>
-              <input id="tgl_lahir" type="date" class="form-control" name="tgl_lahir">
+              <input id="tgl_lahir" type="date" class="form-control" name="tgl_lahir" value="<?php echo $data['tgl_lahir']; ?>">
               <div class="invalid-feedback">
             </div>
           </div>
             <div class="form-group col-6">
               <label class="d-block" for="jk">Jenis Kelamin</label>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" id="jk" value="">
+                <input class="form-check-input" type="radio" id="jk1" name="jk" value="Laki-laki" <?php echo ($data['jk'] == 'Laki-laki') ? " checked" : "" ?>>
                 <label class="form-check-label" for="jk">Laki-laki</label>
               </div>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" id="jk" value="">
+                <input class="form-check-input" type="radio" id="jk2" name="jk" value="Perempuan" <?php echo ($data['jk'] == 'Perempuan') ? " checked" : "" ?>>
                 <label class="form-check-label" for="jk">Perempuan</label>
               </div>
             </div>
@@ -320,31 +327,32 @@
           <div class="row">
             <div class="form-group col-6">
               <label>Status</label>
-              <select class="form-control selectric">
-                <option selected>--Pilih Status--</option>
-                <option>West Java</option>
-                <option>East Java</option>
+              <select class="form-control selectric" name="status">
+                <option value="">--Pilih Status--</option>
+                <option value="Belum Menikah" <?php echo ($data['status'] == 'Belum Menikah') ? " selected" : "" ?>>Belum Menikah</option>
+                <option value="Sudah Menikah" <?php echo ($data['status'] == 'Sudah Menikah') ? " selected" : "" ?>>Sudah Menikah</option>
               </select>
             </div>
             <div class="form-group col-6">
               <label for="agama">Agama</label>
-              <input id="agama" type="text" class="form-control" name="agama">
+              <input id="agama" type="text" class="form-control" name="agama" value="<?php echo $data['agama'] ?>">
               <div class="invalid-feedback">
               </div>
             </div>
           </div>
           <div class="row">
             <div class="form-group col-6">
-              <label>Kelurahan</label>
-              <select class="form-control selectric">
-                <option selected>--Pilih Kelurahan--</option>
-                <option>West Java</option>
+              <label for="kelurahan">Kelurahan</label>
+              <input id="kelurahan" type="text" class="form-control" name="kelurahan" value="<?php echo $data['kelurahan'] ?>">
+              <!-- <select class="form-control selectric">
+                <option value="">--Pilih Kelurahan--</option>
+                <option value="">West Java</option>
                 <option>East Java</option>
-              </select>
+              </select> -->
             </div>
             <div class="form-group col-6">
               <label>Kecamatan</label>
-              <select class="form-control selectric">
+              <select class="form-control selectric" name="kecamatan">
                 <option selected>Banua Lawas</option>
               </select>
             </div>
@@ -353,13 +361,13 @@
           <div class="row">
             <div class="form-group col-6">
               <label for="rt">RT</label>
-              <input id="rt" type="number" class="form-control" name="rt">
+              <input id="rt" type="text" class="form-control" name="rt" value="<?php echo $data['rt'] ?>">
               <div class="invalid-feedback">
               </div>
             </div>
             <div class="form-group col-6">
               <label for="rw">RW</label>
-              <input id="rw" type="number" class="form-control" name="rw">
+              <input id="rw" type="text" class="form-control" name="rw" value="<?php echo $data['rw'] ?>">
               <div class="invalid-feedback">
               </div>
             </div>
@@ -367,12 +375,12 @@
 
           <div class="row">
             <div class="form-group col-6">
-              <label>Alamat Domisili</label>
-              <textarea class="form-control"></textarea>
+              <label for="alamat">Alamat Domisili</label>
+              <input id="alamat" type="text" class="form-control" name="alamat" value="<?php echo $data['alamat'] ?>">
             </div>
             <div class="form-group col-6">
-              <label for="kodepos">Kode Pos</label>
-              <input id="kodepos" type="number" class="form-control" name="kodepos">
+              <label for="kode_pos">Kode Pos</label>
+              <input id="kode_pos" type="number" class="form-control" name="kode_pos" value="<?php echo $data['kode_pos'] ?>">
               <div class="invalid-feedback">
               </div>
             </div>
@@ -382,39 +390,47 @@
           <div class="form-group">
             <label>Upload Foto/Scan Buku Nikah Suami Istri</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="customFile">
-              <label class="custom-file-label" for="customFile">Choose file</label>
-            </div>
-          </div>
-          <div class="form-group">
-            <label>Upload Foto/Scan Akta Mertua dan Orang Tua</label>
-            <div class="custom-file">
-              <input type="file" class="custom-file-input" id="customFile">
-              <label class="custom-file-label" for="customFile">Choose file</label>
+              <input type="file" class="form-control" name="file_buku_nikah">
+              <p class="text-dark">File yang diunggah: <?php echo $data['file_buku_nikah']; ?></p>
+              <img src="assets/<?php echo $data['file_buku_nikah'] ?>" width="100">
+              <p style="color: red">Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .pdf</p>
             </div>
           </div>
           <div class="form-group">
             <label>Upload Foto/Scan Kartu Tanda Penduduk</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="customFile">
-              <label class="custom-file-label" for="customFile">Choose file</label>
+              <input type="file" class="form-control" name="file_ktp">
+              <p class="text-dark">File yang diunggah: <?php echo $data['file_ktp']; ?></p>
+              <img src="assets/<?php echo $data['file_ktp'] ?>" width="100">
+              <p style="color: red">Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .pdf</p>
+            </div>
+          </div>
+          <div class="form-group">
+            <label>Upload Foto/Scan Ijazah Pendidikan Terakhir / SK Kerja</label>
+            <div class="custom-file">
+              <input type="file" class="form-control" name="file_ijazah">
+              <p class="text-dark">File yang diunggah: <?php echo $data['file_ijazah']; ?></p>
+              <img src="assets/<?php echo $data['file_ijazah'] ?>" width="100">
+              <p style="color: red">Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .pdf</p>
             </div>
           </div>
           <div class="form-group">
             <label>Upload Foto/Scan Kartu Keluarga</label>
             <div class="custom-file">
-              <input type="file" class="custom-file-input" id="customFile">
-              <label class="custom-file-label" for="customFile">Choose file</label>
+              <input type="file" class="form-control" name="file_kk">
+              <p class="text-dark">File yang diunggah: <?php echo $data['file_kk']; ?></p>
+              <img src="assets/<?php echo $data['file_kk'] ?>" width="100">
+              <p style="color: red">Ekstensi yang diperbolehkan .png | .jpg | .jpeg | .pdf</p>
             </div>
           </div>
           <div class="form-group">
-            <button type="submit" class="btn btn-success btn-md">
+            <button type="submit" class="btn btn-success btn-md" name="kirim">
               <i class="fas fa-paper-plane"></i> Kirim
             </button>
-            <button type="submit" class="btn btn-warning btn-md">
+            <button type="submit" class="btn btn-warning btn-md" name="simpan_sementara">
               <i class="fas fa-save"></i> Simpan Sementara
             </button>
-            <button type="submit" class="btn btn-danger btn-md">
+            <button type="reset" class="btn btn-danger btn-md">
               <i class="fas fa-window-close"></i> Batal
             </button>
           </div>
