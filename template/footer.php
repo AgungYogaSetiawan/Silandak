3ad3f4f124035d57988f4e2221b9be5d8e17168f
Agung Yoga Setiawan <agungyoga507@gmail.com>
2023-05-10 07:32:20 +0800

    merubah ukuran badge di halaman user

--- template/footer.php
+++ template/footer.php
@@ -53,6 +53,7 @@
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.4.1/js/responsive.bootstrap.min.js"></script>
+<!-- Fungsi custom script js -->
 <script src="assets/js/function.js"></script>
 
 <!-- JS Libraies -->
